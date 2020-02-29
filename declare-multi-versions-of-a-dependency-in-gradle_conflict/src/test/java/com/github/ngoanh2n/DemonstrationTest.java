13e21ed7aee5d018776eacbbd8b44298ce606a60
Ngoan Ho <ngoanh2n@gmail.com>
2020-02-29 16:54:48 +0700

    Format code

--- declare-multi-versions-of-a-dependency-in-gradle_conflict/src/test/java/com/github/ngoanh2n/DemonstrationTest.java
+++ declare-multi-versions-of-a-dependency-in-gradle_conflict/src/test/java/com/github/ngoanh2n/DemonstrationTest.java
@@ -3,10 +3,14 @@ package com.github.ngoanh2n;
 import io.qameta.allure.Allure;
 import org.testng.annotations.Test;
 
+/**
+ * @author ngoanh2n@gmail.com (Ho Huu Ngoan)
+ */
+
 public class DemonstrationTest {
 
     @Test
-    void test(){
+    void test() {
         Allure.step("This is test", () -> {
             System.out.println("Today is 2020-02-28");
         });
